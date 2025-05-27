751a5cddc92f515311f4027ce1239a0216467cf9
root <root@srv838088.hstgr.cloud>
2025-05-27 09:28:20 +0000

    WIP: cambios locales en vite.config y package.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+      port: 4173,
+      host: true, // permite acceso desde fuera de localhost
+      allowedHosts: ['www.yampe.dev', 'yampe.dev']
+    }
 })
